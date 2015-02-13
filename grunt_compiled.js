75960adb84a0581ba414a34209d79cc021fc3212
Armaklan <lionel.zuber@armaklan.org>
2015-02-13 06:05:26 +0100

    Add kill task

--- grunt_compiled.js
+++ grunt_compiled.js
@@ -9,12 +9,18 @@ goog.require('lt.objs.editor.pool');
 goog.require('lt.objs.files');
 goog.require('lt.objs.sidebar.command');
 goog.require('lt.objs.command');
+goog.require('clojure.string');
 goog.require('lt.objs.plugins');
 goog.require('lt.objs.sidebar.command');
+goog.require('clojure.string');
 goog.require('lt.objs.plugins');
 goog.require('lt.objs.editor');
 goog.require('lt.objs.files');
 goog.require('lt.object');
+lt.object.object_STAR_.call(null,new cljs.core.Keyword("lt.plugins.grunt","grunt.client","lt.plugins.grunt/grunt.client",4277577878),new cljs.core.Keyword(null,"tags","tags",1017456523),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"client","client",3951159101),null,new cljs.core.Keyword(null,"grunt.client","grunt.client",1077045947),null], null), null),new cljs.core.Keyword(null,"behaviors","behaviors",607554515),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"start-task","start-task",3689542530),new cljs.core.Keyword(null,"kill","kill",1017196240)], null),new cljs.core.Keyword(null,"name","name",1017277949),"Grunt task",new cljs.core.Keyword(null,"queue","queue",1121848451),cljs.core.PersistentVector.EMPTY);
+lt.plugins.grunt.grunt_client = lt.object.create.call(null,new cljs.core.Keyword("lt.plugins.grunt","grunt.client","lt.plugins.grunt/grunt.client",4277577878));
+lt.plugins.grunt.log = (function log(str){return console.log(str);
+});
 lt.plugins.grunt.current_grunt_file = (function current_grunt_file(){var editor = lt.objs.editor.pool.last_active.call(null);var path = new cljs.core.Keyword(null,"path","path",1017337751).cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"info","info",1017141280).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,editor)));return lt.objs.files.walk_up_find.call(null,path,"Gruntfile.js");
 });
 lt.plugins.grunt.current_modules = (function current_modules(){var editor = lt.objs.editor.pool.last_active.call(null);var path = new cljs.core.Keyword(null,"path","path",1017337751).cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"info","info",1017141280).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,editor)));return lt.objs.files.walk_up_find.call(null,path,"node_modules");
@@ -25,31 +31,65 @@ cli.option("verbose","true");
 return cli;
 });
 lt.plugins.grunt.grunt_task_list = (function grunt_task_list(){try{var cli = lt.plugins.grunt.grunt_cli.call(null);return cli.task._tasks;
-}catch (e8243){var e = e8243;cljs.core.print.call(null,"Impossible de trouver grunt");
+}catch (e7853){var e = e7853;lt.plugins.grunt.log.call(null,"Impossible de trouver grunt");
 return [];
 }});
-lt.plugins.grunt.run_task = (function run_task(task){var cli = lt.plugins.grunt.grunt_cli.call(null);cljs.core.println.call(null,"Grunt : Run task : ",new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(task));
-cli.task.run(new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(task));
+lt.plugins.grunt.run_task = (function run_task(task){var cli = lt.plugins.grunt.grunt_cli.call(null);cli.task.run(new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(task));
 return cli.task.start();
+});
+lt.plugins.grunt.concatener = (function concatener(str1,str2){return clojure.string.join.call(null,cljs.core.concat.call(null,str1,str2));
+});
+lt.plugins.grunt.run_task_process = (function run_task_process(task){var grunt_cmd = lt.plugins.grunt.concatener.call(null,lt.plugins.grunt.current_modules.call(null),"/grunt-cli/bin/grunt --no-color ");var child = require("child_process").exec(lt.plugins.grunt.concatener.call(null,grunt_cmd,new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(task)),((function (grunt_cmd){
+return (function (err,stdout,stderr){if(cljs.core.seq.call(null,err))
+{return cljs.core.println.call(null,err);
+} else
+{return null;
+}
+});})(grunt_cmd))
+);child.stdout.on("data",((function (child,grunt_cmd){
+return (function (data){return lt.plugins.grunt.log.call(null,data);
+});})(child,grunt_cmd))
+);
+child.stderr.on("data",((function (child,grunt_cmd){
+return (function (data){return lt.plugins.grunt.log.call(null,data);
+});})(child,grunt_cmd))
+);
+return child;
 });
 lt.plugins.grunt.get_tasks = (function get_tasks(){var tasks = lt.plugins.grunt.grunt_task_list.call(null);return cljs.core.map.call(null,((function (tasks){
 return (function (key){return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"name","name",1017277949),key,new cljs.core.Keyword(null,"description","description",3584325486),(tasks[key]).info], null);
 });})(tasks))
 ,Object.keys(tasks));
 });
-lt.plugins.grunt.selector = (function selector(opts){var G__8245 = lt.objs.sidebar.command.filter_list.call(null,opts);lt.object.add_behavior_BANG_.call(null,G__8245,new cljs.core.Keyword("lt.plugins.grunt","set-selected","lt.plugins.grunt/set-selected",3339425851));
-return G__8245;
+lt.plugins.grunt.selector = (function selector(opts){var G__7855 = lt.objs.sidebar.command.filter_list.call(null,opts);lt.object.add_behavior_BANG_.call(null,G__7855,new cljs.core.Keyword("lt.plugins.grunt","set-selected","lt.plugins.grunt/set-selected",3339425851));
+return G__7855;
 });
-lt.plugins.grunt.add_selector = (function add_selector(){return lt.plugins.grunt.selector.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"items","items",1114430258),lt.plugins.grunt.get_tasks.call(null),new cljs.core.Keyword(null,"key","key",1014010321),new cljs.core.Keyword(null,"name","name",1017277949),new cljs.core.Keyword(null,"placeholder","placeholder",1612151013),"Grunt target",new cljs.core.Keyword(null,"transform","transform",2066570974),(function (p1__8247_SHARP_,p2__8248_SHARP_,p3__8249_SHARP_,p4__8246_SHARP_){return [cljs.core.str("<p>"),cljs.core.str(new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(p4__8246_SHARP_)),cljs.core.str("</p>"),cljs.core.str("<p class='binding'>"),cljs.core.str(new cljs.core.Keyword(null,"description","description",3584325486).cljs$core$IFn$_invoke$arity$1(p4__8246_SHARP_)),cljs.core.str("</p>")].join('');
+lt.plugins.grunt.add_selector = (function add_selector(){return lt.plugins.grunt.selector.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"items","items",1114430258),lt.plugins.grunt.get_tasks.call(null),new cljs.core.Keyword(null,"key","key",1014010321),new cljs.core.Keyword(null,"name","name",1017277949),new cljs.core.Keyword(null,"placeholder","placeholder",1612151013),"Grunt target",new cljs.core.Keyword(null,"transform","transform",2066570974),(function (p1__7857_SHARP_,p2__7858_SHARP_,p3__7859_SHARP_,p4__7856_SHARP_){return [cljs.core.str("<p>"),cljs.core.str(new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(p4__7856_SHARP_)),cljs.core.str("</p>"),cljs.core.str("<p class='binding'>"),cljs.core.str(new cljs.core.Keyword(null,"description","description",3584325486).cljs$core$IFn$_invoke$arity$1(p4__7856_SHARP_)),cljs.core.str("</p>")].join('');
 })], null));
 });
-lt.plugins.grunt.refresh_task = (function refresh_task(){return lt.object.assoc_in_BANG_.call(null,lt.objs.command.manager,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"commands","commands",4706336250),new cljs.core.Keyword(null,"grunt.default","grunt.default",3655513589),new cljs.core.Keyword(null,"options","options",4059396624)], null),lt.plugins.grunt.add_selector.call(null));
+lt.plugins.grunt.refresh_task = (function refresh_task(){return lt.object.assoc_in_BANG_.call(null,lt.objs.command.manager,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"commands","commands",4706336250),new cljs.core.Keyword(null,"grunt.run","grunt.run",4658105151),new cljs.core.Keyword(null,"options","options",4059396624)], null),lt.plugins.grunt.add_selector.call(null));
 });
-lt.plugins.grunt.__BEH__on_target_select = (function __BEH__on_target_select(this$,v){return lt.objs.sidebar.command.exec_active_BANG_.call(null,v);
+lt.plugins.grunt.__BEH__kill = (function __BEH__kill(this$){var temp__4126__auto__ = new cljs.core.Keyword("lt.plugins.grunt","process","lt.plugins.grunt/process",884763508).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,this$));if(cljs.core.truth_(temp__4126__auto__))
+{var process = temp__4126__auto__;lt.plugins.grunt.log.call(null,"Kill process : ");
+process.kill();
+return lt.object.merge_BANG_.call(null,this$,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword("lt.plugins.grunt","process","lt.plugins.grunt/process",884763508),null], null));
+} else
+{return null;
+}
 });
-lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.grunt","on-target-select","lt.plugins.grunt/on-target-select",2806305431),new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"select","select",4402849902),null], null), null),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.grunt.__BEH__on_target_select);
-lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"grunt.default","grunt.default",3655513589),new cljs.core.Keyword(null,"desc","desc",1016984067),"Grunt: Run Default Task",new cljs.core.Keyword(null,"options","options",4059396624),lt.plugins.grunt.add_selector.call(null),new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.grunt.run_task], null));
-lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"grunt.refresh","grunt.refresh",3196154511),new cljs.core.Keyword(null,"desc","desc",1016984067),"Grunt: Refresh task list",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.grunt.refresh_task], null));
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword(null,"kill","kill",1017196240),new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"kill","kill",1017196240),null], null), null),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.grunt.__BEH__kill);
+lt.plugins.grunt.__BEH__start_task = (function __BEH__start_task(this$,task){cljs.core.println.call(null,"Start task");
+var child = lt.plugins.grunt.run_task_process.call(null,task);return lt.object.merge_BANG_.call(null,this$,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword("lt.plugins.grunt","process","lt.plugins.grunt/process",884763508),child], null));
+});
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword(null,"start-task","start-task",3689542530),new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"start-task","start-task",3689542530),null], null), null),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.grunt.__BEH__start_task);
+lt.plugins.grunt.__BEH__set_selected = (function __BEH__set_selected(this$,v){return lt.objs.sidebar.command.exec_active_BANG_.call(null,v);
+});
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.grunt","set-selected","lt.plugins.grunt/set-selected",3339425851),new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"select","select",4402849902),null], null), null),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.grunt.__BEH__set_selected);
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"grunt.kill","grunt.kill",2314762702),new cljs.core.Keyword(null,"desc","desc",1016984067),"Grunt: Kill task",new cljs.core.Keyword(null,"exec","exec",1017031683),(function (){return lt.object.raise.call(null,lt.plugins.grunt.grunt_client,new cljs.core.Keyword(null,"kill","kill",1017196240));
+})], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"grunt.run","grunt.run",4658105151),new cljs.core.Keyword(null,"desc","desc",1016984067),"Grunt: Run a Task",new cljs.core.Keyword(null,"options","options",4059396624),lt.plugins.grunt.add_selector.call(null),new cljs.core.Keyword(null,"exec","exec",1017031683),(function (task){return lt.object.raise.call(null,lt.plugins.grunt.grunt_client,new cljs.core.Keyword(null,"start-task","start-task",3689542530),task);
+})], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"grunt.load","grunt.load",2314797910),new cljs.core.Keyword(null,"desc","desc",1016984067),"Grunt: Load task list",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.grunt.refresh_task], null));
 }
 
 //# sourceMappingURL=grunt_compiled.js.map
--- src/lt/plugins/ltgrunt.cljs
+++ src/lt/plugins/ltgrunt.cljs
@@ -1,5 +1,6 @@
 (ns lt.plugins.grunt
   (:require [lt.object :as object]
+            [clojure.string :as string]
             [lt.objs.files :as files]
             [lt.objs.plugins :as plugins]
             [lt.objs.editor :as editor]
@@ -8,6 +9,16 @@
             [lt.objs.command :as cmd])
   (:require-macros [lt.macros :refer [behavior]]))
 
+(object/object* ::grunt.client
+                :tags #{:client :grunt.client}
+                :behaviors [:start-task :kill]
+                :name "Grunt task"
+                :queue [])
+
+(def grunt-client (object/create ::grunt.client))
+
+(defn log [str]
+  (.log js/console str))
 
 (defn current-grunt-file []
   (let [editor (pool/last-active)
@@ -32,15 +43,27 @@
     (let [cli (grunt-cli)]
       (.-_tasks (.-task cli)))
   (catch :default e
-    (print "Impossible de trouver grunt")
+    (log "Impossible de trouver grunt")
     #js [])))
 
 (defn run-task [task]
   (let [cli (grunt-cli)]
-    (println "Grunt : Run task : " (:name task))
     (.run (.-task cli) (:name task))
     (.start (.-task cli))
     ))
+
+(defn concatener [str1 str2]
+  (string/join (concat str1 str2)))
+
+(defn run-task-process [task]
+  (let [grunt-cmd (concatener (current-modules) "/grunt-cli/bin/grunt --no-color ")]
+    (let [child (.exec (js/require "child_process")
+           (concatener grunt-cmd (:name task))
+           (fn [err stdout stderr]
+             (when (seq err) (println err))))]
+      (.on (.-stdout child) "data" (fn [data] (log data)))
+      (.on (.-stderr child) "data" (fn [data] (log data)))
+      child)))
 
 (defn get-tasks []
   (let [tasks (grunt-task-list)]
@@ -58,19 +81,40 @@
                               "<p class='binding'>" (:description %4) "</p>")}))
 
 (defn refresh-task []
-  (object/assoc-in! cmd/manager [:commands :grunt.default :options] (add-selector)))
+  (object/assoc-in! cmd/manager [:commands :grunt.run :options] (add-selector)))
 
+(behavior :kill
+          :triggers #{:kill}
+          :reaction (fn [this]
+                      (when-let [process (::process @this)]
+                        (log "Kill process : ")
+                        (.kill process)
+                        (object/merge! this {::process nil}))))
 
-(behavior ::on-target-select
+(behavior :start-task
+          :triggers #{:start-task}
+          :reaction (fn [this task]
+                      (println "Start task")
+                      (let [child (run-task-process task)]
+                        (object/merge! this {::process child})
+                        )))
+
+(behavior ::set-selected
           :triggers #{:select}
           :reaction (fn [this v]
                       (sidebar/exec-active! v)))
 
-(cmd/command {:command :grunt.default
-              :desc "Grunt: Run Default Task"
-              :options (add-selector)
-              :exec run-task})
+(cmd/command {:command :grunt.kill
+              :desc "Grunt: Kill task"
+              :exec (fn []
+                      (object/raise grunt-client :kill))})
 
-(cmd/command {:command :grunt.refresh
-              :desc "Grunt: Refresh task list"
+(cmd/command {:command :grunt.run
+              :desc "Grunt: Run a Task"
+              :options (add-selector)
+              :exec (fn [task]
+                      (object/raise grunt-client :start-task task))})
+
+(cmd/command {:command :grunt.load
+              :desc "Grunt: Load task list"
               :exec refresh-task})
